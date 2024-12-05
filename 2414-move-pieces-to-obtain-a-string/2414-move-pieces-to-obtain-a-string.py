38ce396e0defc1dc51c63568c438c2cba9b0a96a
AnouMer <47783955+Anou26@users.noreply.github.com>
2024-12-04 20:04:13 -0500

    Time: 61 ms (99.19%), Space: 17.7 MB (46.11%) - LeetHub

---
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        if start == target:
+            return True
+        pending_L = 0   
+        waiting_R = 0    
+
+        for curr, goal in zip(start, target):
+            if curr == 'R':
+                if pending_L > 0:
+                    return False
+                waiting_R += 1  
+            if goal == 'L':
+                if waiting_R > 0:
+                    return False
+                pending_L += 1
+            if goal == 'R':
+                if waiting_R == 0:
+                    return False
+                waiting_R -= 1 
+            if curr == 'L':
+                if pending_L == 0:
+                    return False
+                pending_L -= 1    
+        return pending_L == 0 and waiting_R == 0
+        
